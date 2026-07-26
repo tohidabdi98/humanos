48c1f6d90901fd2ecc3c57738a013caa59837749
Tohid Abdi <tohidabdi98@gmail.com>
2026-07-27 03:17:02 +0330

    designing the identity system

---
+++ backend/humanos-domain/src/main/java/com/humanos/domain/identity/User.java
@@ -0,0 +1,55 @@
+package com.humanos.domain.identity;
+
+import java.util.UUID;
+
+public class User {
+    private final UUID id;
+    private String username;
+    private String email;
+
+    private User(
+            UUID id,
+            String username,
+            String email
+    ) {
+
+        if(username == null || username.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Username cannot be empty"
+            );
+        }
+
+        if(email == null || email.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Email cannot be empty"
+            );
+        }
+
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public static User create(
+            String username,
+            String email
+    ) {
+        return new User(
+                UUID.randomUUID(),
+                username,
+                email
+        );
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
